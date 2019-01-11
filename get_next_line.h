a142cf6f063b905d487da1f6bc72545d1cc8bd85
Lu-nung  Lee <llee@e1z2r2p2.42.us.org>
2019-01-11 11:35:43 -0800

    added header file

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llee <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/29 14:06:09 by llee              #+#    #+#             */
+/*   Updated: 2018/11/14 20:00:39 by llee             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 2056
+
+# include "libft/libft.h"
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+
+int	copy_to_line(char **array, char **line, int fd, int file);
+int	get_next_line(const int fd, char **line);
+
+#endif
